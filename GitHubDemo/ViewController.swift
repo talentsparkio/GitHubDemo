28210e2a3d6e947bfe992ba9d8c743e6147bfc71
Nicholas Chen <nchen.dev@mac.com>
2015-08-03 22:33:58 -0700

    Demo of how to do things programmatically

--- GitHubDemo/ViewController.swift
+++ GitHubDemo/ViewController.swift
@@ -9,17 +9,46 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var hello: UILabel?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.blueColor()
+        view.backgroundColor = UIColor.darkGrayColor()
+        
+        hello = UILabel(frame: CGRectMake(27.5, 100, 320, 30))
+        if let hello = hello {
+            hello.textAlignment = NSTextAlignment.Center
+            hello.textColor = UIColor.whiteColor()
+            hello.font = UIFont(name: "System", size: 20)
+            hello.text = "Hello, World!"
+            view.addSubview(hello)
+        }
+        
+        let world = UIImageView(frame: CGRectMake(59.5, 130, 256, 256))
+        world.image = UIImage(named:"World")
+        world.clipsToBounds = true
+        view.addSubview(world)
+        
+        let button = UIButton(frame: CGRectMake(27.5, 400, 320, 30))
+        button.titleLabel?.textColor = UIColor.whiteColor()
+        button.titleLabel?.font = UIFont.systemFontOfSize(18)
+        button.setTitle("Click me", forState: UIControlState.Normal)
+        button.addTarget(self, action: "buttonClicked", forControlEvents: UIControlEvents.TouchUpInside)
+        view.addSubview(button)
     }
-
+    
+    func buttonClicked() {
+        hello?.text = "Hello, Clicker!"
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
