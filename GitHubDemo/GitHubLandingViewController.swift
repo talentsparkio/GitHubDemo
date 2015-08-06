c312848f10f4a417895132121bb5d4b992f22a17
Nicholas Chen <nchen.dev@mac.com>
2015-08-06 01:00:22 -0700

    Displays the organization info

--- GitHubDemo/GitHubLandingViewController.swift
+++ GitHubDemo/GitHubLandingViewController.swift
@@ -17,6 +17,18 @@ class GitHubOrgLandingViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        GitHubOrganization().fetchOrganizationDetails { (org: GitHubOrganization.Organization) -> () in
+            self.organizationLabel.text = org.organizationName
+            self.descriptionLabel.text = org.description
+            self.locationLabel.text = org.location
+            
+            if let url = NSURL(string: org.avatarURL) {
+                if let data = NSData(contentsOfURL: url){
+                    self.avatar.contentMode = UIViewContentMode.ScaleAspectFit
+                    self.avatar.image = UIImage(data: data)
+                }
+            }
+        }
     }
     
     override func didReceiveMemoryWarning() {
